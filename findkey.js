284c9af183cfa0bbcd2fcc2fbb5451409d696dce
Lee Benzacar <Lmbenzacar@gmail.com>
2022-01-14 07:22:11 +0000

    findkey added

---
+++ findkey.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => Object.keys(object).find(key => callback(object[key]));

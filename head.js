43985ae9ce87e17c1657fa07e80a27c5c45cf4d1
Lee Benzacar <LMBenzacar@gmail.com>
2022-01-17 20:35:35 +0000

    Module refactoring

--- head.js
+++ head.js
@@ -1,18 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    return console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assertEqual = require('./assertEqual');
 
 function head(array) {
 let item = array[0];
 return item;
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;

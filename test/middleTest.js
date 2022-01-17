52b8a9dc8e2bf9326e90fd25c0515a3a4c53a480
Lee Benzacar <LMBenzacar@gmail.com>
2022-01-17 23:50:41 +0000

    added tailtesting and middletesting to mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,10 @@
-const assertArraysEqual = require('../assertArraysEqual');
+
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-console.log(assertArraysEqual(middle([1,4,7,12,16]), [7]));
+describe("#middle", () => {
+  it("returns 7 for [1, 4, 7, 12, 16]", () => {
+    assert.deepEqual(middle([1,4,7,12,16]), [7]);
+  });
+ 
+});
